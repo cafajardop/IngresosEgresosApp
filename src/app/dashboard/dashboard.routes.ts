592199aee097b0baf3a8b990e79e82eb8a244b04
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-06-18 14:40:13 -0500

    commit ingresos Egresos

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+import { NgModule } from '@angular/core';
+
+export const dashboardRoutes: Routes= [
+    {path:'', component: EstadisticaComponent},
+    {path:'ingreso-egreso', component: IngresoEgresoComponent},
+    {path:'detalle', component: DetalleComponent},
+];
